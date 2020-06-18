89aeadd920985299851832df6f1dcfb1ad35c9cd
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-18 17:04:09 +0200

    Use black to format code

--- everycheese/users/views.py
+++ everycheese/users/views.py
@@ -33,17 +33,12 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     # Send the User Back to Their Own Page after a
     #   successful Update
     def get_success_url(self):
-        return reverse(
-            "users:detail",
-            kwargs={'username': self.request.user.username},
-        )
+        return reverse("users:detail", kwargs={"username": self.request.user.username},)
 
     def get_object(self):
         # Only Get the User Record for the
         #   User Making the Request
-        return User.objects.get(
-            username=self.request.user.username
-        )
+        return User.objects.get(username=self.request.user.username)
 
 
 user_update_view = UserUpdateView.as_view()
@@ -53,10 +48,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse(
-            "users:detail",
-            kwargs={"username": self.request.user.username},
-        )
+        return reverse("users:detail", kwargs={"username": self.request.user.username},)
 
 
 user_redirect_view = UserRedirectView.as_view()
